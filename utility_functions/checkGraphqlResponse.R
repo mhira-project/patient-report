bcc2e820ddb4049bd6248134969d65b1df6d2565
RonanZimmermann <zimmermann.ronan@gmail.com>
2022-07-20 16:31:46 +0200

    correct error messages

--- utility_functions/checkGraphqlResponse.R
+++ utility_functions/checkGraphqlResponse.R
@@ -1,8 +1,6 @@
 checkGraphqlResponse = function(response, session){
-
-
   
-# Terminate session if response doesn't contain data     
+# Terminate session if response doesn't contain the required data     
 if(is_empty(response$data)){
   showNotification(
     response$errors$message,
@@ -12,14 +10,15 @@ if(is_empty(response$data)){
 
 if(is_empty(response$data$generatePatientReport$patient)){
   showNotification(
-    "No paitent was found. The reporting app will shut down.",
+    "No patient was found. The reporting app will shut down.",
     type = "error",
     duration = 20)
   session$close()}
 
 if(is_empty(response$data$generatePatientReport$assessments)){
   showNotification(
-    "No assessment was found. Please make sure the patient has assessments. The reporting app will shut down.",
+    "No assessment was found. Please make sure the patient has assessments. 
+     The reporting app will shut down.",
     type = "error",
     duration = 20)
   session$close()}
@@ -33,21 +32,21 @@ if(is_empty(response$data$generatePatientReport$answeredQuestionnaires)){
 
 if(is_empty(response$data$generatePatientReport$questionnaireScripts)){
   showNotification(
-    "No completed questionnaire was found. The reporting app will shut down.",
+    "No questionnaire scripts were found for the questionnaires completed by this patient.
+     You need to add these evaluation routines to your questionnaires.
+     The reporting app will shut down.",
     type = "error",
     duration = 20)
   session$close()}
  
  if(is_empty(response$data$generatePatientReport$questionnaireScripts %>%
              filter(name == "scales_table" | name == "scales_function"))){
-    showNotification(
-      "No scale_script or scales_table found.
-      You need to add these evaluation routines to your questionnaires",
+   showNotification(
+      "No scale_script or scales_table for the questionnaires done by this patient found.
+      You need to add these evaluation routines to your questionnaires.",
       type = "error",
       duration = 20)
     session$close()}
 
-  
 
-  
   }
