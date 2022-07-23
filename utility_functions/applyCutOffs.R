64b70b7452dcd373e617ee9c85af31f370400b63
RonanZimmermann <zimmermann.ronan@gmail.com>
2022-07-23 14:35:06 +0200

    applycutoff if there is no cutoff file

--- utility_functions/applyCutOffs.R
+++ utility_functions/applyCutOffs.R
@@ -2,11 +2,19 @@ applyCutOffs = function(scales, questionnaireScripts){
   print("applyCutOffs")
   
   
-  qS = questionnaireScripts %>%
-    filter(name == "cutoffs")
   
-
-cutoffs = data.frame()
+  cutoffColnames = c('scale', 'low_cut', 'high_cut', 'level', 'warning', 'text_order', 'interpretation', 'recommendation')
+  
+  qS = questionnaireScripts %>%
+  filter(name == "cutoffs")
+  
+  if(nrow(qS) == 0){
+    df = scales %>% full_join( as_tibble(matrix(nrow = 0, ncol = length(cutoffColnames) + 1), .name_repair = ~ c('questionnaireId',cutoffColnames))) 
+    return(df)}
+  
+  
+  
+  cutoffs = data.frame()
 
   for (r in 1:nrow(qS)){
    
@@ -15,18 +23,16 @@ cutoffs = data.frame()
     
   }
  
+requiredColumsCutoffs =  all(cutoffColnames %in% colnames(cutoffs))
 
-cutoff_colnames = c('questionnaireVersionId', 'scale', 'low_cut', 'high_cut', 'level', 'warning', 'text_order', 'interpretation', 'recommendation')
-required_colums_cutoffs =  all(cutoff_colnames %in% colnames(cutoffs))
-
-if(!required_colums_cutoffs){
+if(!requiredColumsCutoffs){
   showNotification(
     "A cutoff file used in at least one of the questionnaires is missing one or more of the required columns",
     type = "error",
     duration = 20)}
 
 
-cutoffs = cutoffs %>% select(all_of(cutoff_colnames)) 
+cutoffs = cutoffs %>% select(c("questionnaireVersionId", all_of(cutoffColnames))) # if the cutoff files change, this needs to be adapted
 
 
 result = scales %>% 
